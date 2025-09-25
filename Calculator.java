8c139817e6149066612658eb87d7802833928a6a
vyletran1403 <14032004tranlevy@gmail.com>
2025-09-26 00:04:49 +0700

    Implement Multiply function
    áda Conflicts:

--- Calculator.java
+++ Calculator.java
@@ -6,10 +6,15 @@ public class Calculator {
 
     private Double calculate(SIGN sign, Double arg1, Double arg2) {
         switch (sign) {
+<<<<<<< HEAD
             case PLUS:
                 return arg1 + arg2; // branch implement_plus
             case MINUS:
                 return arg1 - arg2; // branch implement_minus
+=======
+            case MULTIPLY:
+                return arg1 * arg2; // branch implement_multiply
+>>>>>>> b19d495 (Implement Multiply function)
             default:
                 return 0d;
         }
@@ -17,7 +22,11 @@ public class Calculator {
 
     public static void main(String[] args) {
         Calculator c = new Calculator();
+<<<<<<< HEAD
         System.out.println("Plus: " + c.calculate(SIGN.PLUS, 5.0, 3.0));
         System.out.println("Minus: " + c.calculate(SIGN.MINUS, 5.0, 3.0));
+=======
+        System.out.println("Multiply: " + c.calculate(SIGN.MULTIPLY, 5.0, 3.0));
+>>>>>>> b19d495 (Implement Multiply function)
     }
 }
